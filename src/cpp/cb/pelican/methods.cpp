41584de3259d987b59a4b9d2c3630b783fe27f9f
Tong Zhang <warriorlance@gmail.com>
2014-07-17 20:53:10 +0800

    add maxExAmp and maxEyAmp variables of class FELNumerical

--- src/cpp/cb/pelican/methods.cpp
+++ src/cpp/cb/pelican/methods.cpp
@@ -41,6 +41,10 @@ FELAnalysis::FELAnalysis(undulator &unduP, electronBeam &elecP, FELradiation &ra
     Psat  = 1.6*rho1D*Lg1D*Lg1D/(Lg3D*Lg3D)*gamma0*0.511*current/1000.0; // GW
 }
 
+FELAnalysis::~FELAnalysis()
+{
+}
+
 double FELAnalysis::get_FELparameter1D()
 {
     return rho1D;
@@ -66,7 +70,6 @@ double FELAnalysis::get_FELsatpower()
     return Psat;
 }
 
-
 FELNumerical::FELNumerical(seedfield &seedP, undulator &unduP, electronBeam &elecP, FELradiation &radiP, controlpanel &contP)
 {
     seedEx  = seedP.get_Ex           ();
@@ -84,6 +87,18 @@ FELNumerical::FELNumerical(seedfield &seedP, undulator &unduP, electronBeam &ele
     npart   = contP.get_npart        ();
 }
 
+FELNumerical::~FELNumerical()
+{
+    delete[] psi;
+    delete[] gam;
+    delete[] zposArr;
+    delete[] bfArr;
+    delete[] K0Arr;
+    delete[] JJArr;
+    delete[] ExArr;
+    delete[] EyArr;
+}
+
 void FELNumerical::generateDistribution(double minpsi, double maxpsi)
 {
     psi = new double [npart];
@@ -97,7 +112,7 @@ void FELNumerical::generateDistribution(double minpsi, double maxpsi)
 
 void FELNumerical::initParams()
 {
-    au     = 0.934*bfield*100*lambdau/sqrt(2);          // normalized undulator parameter, calculated from the input parameters read from namelist file
+    au     = 0.934*bfield*100*lambdau/sqrt(2);      // normalized undulator parameter, calculated from the input parameters read from namelist file
     ku     = 2*PI/lambdau;                          // undulator wave number
     omegas = 2*PI*C0/lambdas;                       // radiation angular frequency
     gammar = sqrt(lambdau*(1+au*au)/2.0/lambdas);   // resonant gamma
@@ -116,7 +131,7 @@ void FELNumerical::initParams()
     EyArr   = new std::complex <double> [totalIntSteps];
 
     double btmp;
-    for(int i = 0; i < totalIntSteps; i++)
+    for(unsigned int i = 0; i < totalIntSteps; i++)
     {
         K0Arr[i] = au*sqrt(2);
         btmp = K0Arr[i]*K0Arr[i]/(4.0+2.0*K0Arr[i]*K0Arr[i]);
@@ -124,12 +139,15 @@ void FELNumerical::initParams()
     }
 }
 
-void FELNumerical::FELsolverSingleFrequency1D()
+
+void FELNumerical::FELsolverSingleFrequency1D0()
 {
     std::complex <double> icplx(0, 1), tmpcplx, Ex(seedEx), j1;
 
     for(unsigned int intzstep = 0; intzstep < totalIntSteps; intzstep++)
     {
+        zposArr[intzstep] = intzstep*ndelz;
+        ExArr[intzstep]   = Ex;
         j1 = 2*j0*meanexph(psi, npart, 1);
         for(unsigned int i = 0; i < npart; i++)
         {
@@ -138,15 +156,152 @@ void FELNumerical::FELsolverSingleFrequency1D()
             psi[i] += 2*ku*(gam[i]/gammar-1.0)*ndelz;
         }
         Ex -= coef3*K0Arr[intzstep]*JJArr[intzstep]*j1*ndelz;
-        zposArr[intzstep] = intzstep*ndelz;
-        ExArr[intzstep]   = Ex;
     }
+}
+
+void FELNumerical::FELsolverSingleFrequency1D(std::string method)
+{
+    std::complex <double> Ex0(seedEx), j1;
+    
+    maxExAmp = 0;
+    maxEyAmp = 0;
+
+    if (method.compare("RK4") == 0)
+    {
+        //!< RK4 by default
+        double k1, k2, k3 ,k4;
+        double l1, l2, l3, l4;
+        std::complex <double> m1, m2, m3, m4;
+        double h  = ndelz;
+        double z0 = 0;
+
+        for(unsigned int intzstep = 0; intzstep < totalIntSteps; intzstep++)
+        {
+            zposArr[intzstep] = z0;
+            ExArr  [intzstep] = Ex0;
+
+            z0 += h;
+            j1 = 2*j0*meanexph(psi, npart, 1);
+
+            m1 = odef3(j1, intzstep);
+            m2 = m1;
+            m3 = m1;
+
+            for(unsigned int i = 0; i < npart; i++)
+            {
+                k1 = odef1(psi[i], Ex0, j1, intzstep);
+                l1 = odef2(gam[i]);
+
+                k2 = odef1(psi[i]+0.5*h*l1, Ex0+0.5*h*m1, j1, intzstep);
+                l2 = odef2(gam[i]+0.5*h*k1);
+
+                k3 = odef1(psi[i]+0.5*h*l2, Ex0+0.5*h*m2, j1, intzstep);
+                l3 = odef2(gam[i]+0.5*h*k2);
+
+                k4 = odef1(psi[i]+1.0*h*l3, Ex0+1.0*h*m3, j1, intzstep);
+                l4 = odef2(gam[i]+1.0*h*k3);
+
+                gam[i] += h*(k1+2.0*k2+2.0*k3+k4)/6.0;
+                psi[i] += h*(l1+2.0*l2+2.0*l3+l4)/6.0;
+            }
+            Ex0 += h*(m1+2.0*m2+2.0*m3+m4)/6.0;
+            if (abs(Ex0) > maxExAmp) maxExAmp = abs(Ex0);
+        }
+    }
+    else if (method.compare("EU1") == 0)
+    {
+        //!< EU1
+        double k1;
+        double l1;
+        std::complex <double> m1;
+        double h  = ndelz;
+        double z0 = 0;
+
+        for(unsigned int intzstep = 0; intzstep < totalIntSteps; intzstep++)
+        {
+            zposArr[intzstep] = z0;
+            ExArr  [intzstep] = Ex0;
+
+            z0 += h;
+            j1 = 2*j0*meanexph(psi, npart, 1);
+
+            m1 = odef3(j1, intzstep);
+
+            for(unsigned int i = 0; i < npart; i++)
+            {
+                k1 = odef1(psi[i], Ex0, j1, intzstep);
+                l1 = odef2(gam[i]);
+
+                gam[i] += h*k1;
+                psi[i] += h*l1;
+            }
+            Ex0 += h*m1;
+            if (abs(Ex0) > maxExAmp) maxExAmp = abs(Ex0);
+        }
+    }
+    else if (method.compare("EU2") == 0)
+    {
+        //!< EU2
+        double k1, k2;
+        double l1, l2;
+        std::complex <double> m1, m2;
+        double h  = ndelz;
+        double z0 = 0;
+
+        for(unsigned int intzstep = 0; intzstep < totalIntSteps; intzstep++)
+        {
+            zposArr[intzstep] = z0;
+            ExArr  [intzstep] = Ex0;
+
+            z0 += h;
+            j1 = 2*j0*meanexph(psi, npart, 1);
+
+            m1 = odef3(j1, intzstep);
+            m2 = m1;
+
+            for(unsigned int i = 0; i < npart; i++)
+            {
+                k1 = odef1(psi[i], Ex0, j1, intzstep);
+                l1 = odef2(gam[i]);
+
+                k2 = odef1(psi[i]+h*l1, Ex0+h*m1, j1, intzstep);
+                l2 = odef2(gam[i]+h*k1);
+
+                gam[i] += h*(k1+k2)/2.0;
+                psi[i] += h*(l1+l2)/2.0;
+            }
+            Ex0 += h*(m1+m2)/2.0;
+            if (abs(Ex0) > maxExAmp) maxExAmp = abs(Ex0);
+        }
+    }
+    else
+    {
+        std::cout << "Please define correct method (EU1|EU2|RK4)." << std::endl;
+        exit(1);
+    }
+}
+
+double FELNumerical::odef1(double psi1, std::complex <double> Ex1, std::complex <double> j1, unsigned int idx)
+{
+    std::complex <double> icplx(0, 1), tmpcplx;
+    tmpcplx = -coef1*(K0Arr[idx]*JJArr[idx]*Ex1/2.0/gammar-icplx*coef2*j1)*exp(icplx*psi1);
+    return tmpcplx.real();
+}
+
+double FELNumerical::odef2(double gam1)
+{
+    return 2*ku*(gam1/gammar-1);
+}
+
+std::complex <double> FELNumerical::odef3(std::complex <double> j1, unsigned int idx)
+{
+    return -coef3*K0Arr[idx]*JJArr[idx]*j1;
 }
 
 void FELNumerical::dumpResults()
 {
     std::ofstream out("tmpout");
-    for(int i= 0; i < totalIntSteps; i++)
+    for(unsigned int i= 0; i < totalIntSteps; i++)
     {
         out << zposArr[i] << " " << 0.5*pow(abs(ExArr[i]),2)*eps0*C0*PI*sigmax*sigmax << std::endl;
     }
@@ -167,14 +322,22 @@ unsigned int FELNumerical::get_npart()
     return npart;
 }
 
+double FELNumerical::get_maxExAmp()
+{
+    return maxExAmp;
+}
 
+double FELNumerical::get_maxEyAmp()
+{
+    return maxEyAmp;
+}
 
 // global functions
 double* normrand(int N, double mu, double sigma)
 {
     gsl_rng *r;
     r = gsl_rng_alloc(gsl_rng_taus);
-    gsl_rng_set(r, time(NULL));
+    //!<gsl_rng_set(r, time(NULL));
     double *distnum = new double [N];
     for(int i = 0; i < N; i++)
         distnum[i] = mu + gsl_ran_gaussian(r, sigma);
--- src/cpp/cb/pelican/methods.h
+++ src/cpp/cb/pelican/methods.h
@@ -1,21 +1,23 @@
 #ifndef METHODS_H_
 #define METHODS_H_
 
-/*************************************************************************\
+/** \brief define classes/functions to handle FEL physics related problems.
  *
- * define classes/functions to handle FEL physics related problems
  * class:
  *  1 FELAnalysis : FEL formulae for analytical calculation
  *  2 FELNumerical: Numerical simulations for FEL physics
  * function:
  *  1 *normrand() : generate random distribution with Gaussian distribution
- * created time: Jul. 1st, 15:53, 2014
- * last updated: Jul. 2nd, 10:42, 2014
-\*************************************************************************/
+ *  2 meanexph()  : return <exp(-1i*h*psi)>, double complex type
+ *
+ * created time: Jul. 1st,  15:53, 2014
+ * last updated: Jul. 17th, 19:41, 2014
+ */
 
 #include "constants.h"
 #include "elements.h"
 #include <complex>
+#include <string>
 
 class FELAnalysis : public physicalConstants
 {
@@ -23,9 +25,11 @@ class FELAnalysis : public physicalConstants
         double gamma0, sigmag0, emitn, avgbeta, lambdau, current, lambdas, bfield;
         double sigmax, au, b, JJ, rho1D, rho3D, Lg1D, Lg3D, etad, etae, etag, CapG, Psat;
     public:
-        // class initialization to calculate all physics objectives:
-        // 1D and 3D FEL parameters and gain length, saturation power, as well.
+        //!< class initialization to calculate all physics objectives:
+        //!< 1D and 3D FEL parameters and gain length, saturation power, as well.
         FELAnalysis(undulator &unduP, electronBeam &elecP, FELradiation &radiP);
+        ~FELAnalysis();
+
         double get_FELparameter1D ();
         double get_FELparameter3D ();
         double get_FELgainlength1D();
@@ -39,44 +43,56 @@ class FELNumerical : public physicalConstants
         unsigned int npart, nstep, num;
         double gamma0, sigmag0, emitn, avgbeta, lambdau, current, lambdas, bfield;
         std::complex <double> seedEx, seedEy;
-        double *psi, *gam; // longitudinal phase space, phase [rad] and energy [gamma]
-        double *zposArr;    // longitudinal undulator position, [m]
-        double *bfArr;      // bunching factor
-        std::complex <double> *ExArr, *EyArr; // electric field in x and y, [v/m]
+        double *psi, *gam;  //!< longitudinal phase space, phase [rad] and energy [gamma]
+        double *zposArr;    //!< longitudinal undulator position, [m]
+        double *bfArr;      //!< bunching factor
+        std::complex <double> *ExArr, *EyArr; //!< electric field in x and y, [v/m]
+        double maxExAmp, maxEyAmp;
 
-        // for numerical calculation initialization
+        //!< for numerical calculation initialization
         double coef1, coef2, coef3, gammar, omegas, au, ku, sigmax, j0, ndelz;
         unsigned int totalIntSteps;
-        double *K0Arr; // undulator parameter array, desinged for including errores
-        double *JJArr; // coupled bessel factor array according to K0Arr
+        double *K0Arr; //!< undulator parameter array, desinged for including errores
+        double *JJArr; //!< coupled bessel factor array according to K0Arr
 
     public:
-        // class initialization with input parameters
+        //!< class initialization with input parameters
         FELNumerical(seedfield &seedP, undulator &unduP, electronBeam &elecP, FELradiation &radiP, controlpanel &contP);
+        ~FELNumerical();
 
         unsigned int get_npart();
         double* get_psi();
         double* get_gam();
+        double get_maxExAmp();
+        double get_maxEyAmp();
 
-        // generate initial electron longitudinal distribution
+        //!< generate initial electron longitudinal distribution
         void generateDistribution(double minpsi, double maxpsi);
 
-        // parameters initialization for numerical calculation
+        //!< parameters initialization for numerical calculation
         void initParams();
 
-        // FEL numerical calculation, single frequency in one-dimensional
-        void FELsolverSingleFrequency1D();
+        void FELsolverSingleFrequency1D0();
+        //!< FEL numerical calculation, single frequency in one-dimensional
+        //!< by default Runge-Kutta algorithm is applied,
+        //!< or define key word method as "EU1" or "EU2"
+        void FELsolverSingleFrequency1D(std::string method = "RK4");
 
-        // dump numerical simulation results
+        //!< functions defined for ODE solver
+        double odef1(double psi1, std::complex <double> Ex1, std::complex <double> j1, unsigned int idx);
+        double odef2(double gam1);
+        std::complex <double> odef3(std::complex <double> j1, unsigned int idx);
+
+        //!< dump numerical simulation results
         void dumpResults();
 };
 
-// generate Gaussian distribution, average value of mu,
-// standard deviation value of sigma
+
+//!< generate Gaussian distribution, average value of mu,
+//!< standard deviation value of sigma
 double* normrand(int N, double mu, double sigma);
 
-// return the abs of average of a exp(double) array,
-// i.e. <exp(-1i*h*psi)>
+//!< return the abs of average of a exp(double) array, i.e. <exp(-1i*h*psi)>
 std::complex <double> meanexph(double* &a, unsigned int n, int h);
 
-#endif // METHODS_H_
+#endif //!< METHODS_H_
