0a05104a308d72271218c78cc2cdafa05c8b7ac1
Tong Zhang <warriorlance@gmail.com>
2017-03-17 11:14:59 -0400

    Tidy up source.

---
+++ pelican/constants.h
@@ -0,0 +1,116 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#include <vector>
+#include <string>
+
+/** \brief define classes: physical constants and default input parameters.
+ *
+ * created time: Jul. 1st,  15:48, 2014
+ * last updated: Jul. 17st, 20:06, 2014
+ */
+
+// integration methods:
+// RK4: Runge-Kutta 4th
+// EU1: Euler 1st
+// EU2: Euler 2nd
+enum intMethods 
+{
+    RK4 = 0,
+    EU1 = 1,
+    EU2 = 2
+};
+
+class physicalConstants
+{
+    protected:
+        double PI, eps0, mu0, M0, E0, C0, IA;
+        struct coefsXie{
+            double a1,  a2,  a3,  a4,  a5,  a6,
+                   a7,  a8,  a9,  a10, a11, a12,
+                   a13, a14, a15, a16, a17, a18,
+                   a19;
+            } coefs;
+
+    public:
+        physicalConstants();
+        void info(); //!< show all the physical constants
+};
+
+class defaultParams
+{
+    protected:
+        //!< paramlist: structure of default input parameters list
+        struct paramlist{
+            //!< seedfield
+            std::string seedEx;
+            std::string seedEy;
+
+            //!< undulator
+            double       undulatorField;
+            double       undulatorPeriod;
+            double       undulatorDeltz;
+            unsigned int undulatorNum;
+
+            //!< electron beam
+            double electronAvgBetaFunc;
+            double electronCentralEnergy;
+            double electronEnergySpread;
+            double electronEmitnx;
+            double electronEmitny;
+            double electronPeakCurrent;
+
+            //!< FEL radiation
+            double FELwavelength;
+
+            //!< control panel
+            unsigned int cpnpart;
+            intMethods cpmethod;
+            std::string cpoutfile;
+            std::string cpparfile;
+            std::string cpdisfile;
+            int cpparflag;
+            int cpdisflag;
+            int cppardelz;
+            int cpanaflag;
+
+            //!< scan panel
+            bool spsflag, speflag;
+            std::string spparam, spobjfunc;
+            double spvbegin, spvend, spvstep;
+            std::string spoutfile;
+            
+        } dparams;
+
+        //!< other defined parameters
+        struct otherparams{
+            std::vector <std::string> allowedScanParams; //!< allowed scan parameters
+            std::vector <std::string> allowedObjeParams; //!< allowed objective parameters
+        } oparams;
+
+    public:
+        defaultParams();
+        //~defaultParams();
+};
+
+// define electric field class
+class efield
+{
+    private:
+        double realpart, imagpart;
+//        double phase, amplitude;
+    public:
+        efield(); // default constructor,(0,0)
+        efield(std::string str);
+        efield(double &x, double &y); // (x,y)
+        efield(efield &x);
+        void set_efield(double &x, double &y);
+        efield get_efield(); // return efield class type
+        double get_real();
+        double get_imag();
+        double get_phase();
+        double get_amplitude(); // return |Ex|
+        double get_amplitude2(); // return |Ex|^2
+};
+
+#endif //!< CONSTANTS_H_
---
+++ pelican/main.cpp
@@ -0,0 +1,94 @@
+#include "readinput.h"
+#include "elements.h"
+#include "methods.h"
+#include <cstdlib>
+#include <iostream>
+
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    //!< read input parameters from external namelist file
+    std::map <std::string, std::string> varlist; //!< namelist map variable
+    std::ifstream infile;
+    infile.open(argv[1]);
+    if (!infile.good())
+    {
+        std::cout << "File open error!" << std::endl;
+        exit(1);
+    }
+    //!< store all (name,value) pairs into map variable: varlist
+    getfield(infile, varlist);
+    //!< show all the read parameters
+    //!<printlist(varlist);
+    infile.close();
+
+    //!< set up elements with varlist,
+    //!< or use the default parameters defined in class defaultParams, which is defined in constants.{h,cpp}
+/*
+    // by default
+    seedfield       seedfieldParams;
+    undulator       undulatorParams;
+    electronBeam electronBeamParams;
+    FELradiation FELradiationParams;
+    controlpanel controlpanelParams;
+*/
+
+    //!< read from varlist
+    seedfield       seedfieldParams(varlist);
+    undulator       undulatorParams(varlist);
+    electronBeam electronBeamParams(varlist);
+    FELradiation FELradiationParams(varlist);
+    controlpanel controlpanelParams(varlist);
+    scanpanel       scanpanelParams(varlist);
+/*
+    // print information of each element
+    seedfieldParams.info();
+    undulatorParams.info();
+    electronBeamParams.info();
+    FELradiationParams.info();
+    controlpanelParams.info();
+*/
+
+    
+    if (controlpanelParams.get_anaflag() == 1) //!< do FEL analytical calculation
+    {
+        FELAnalysis FELAnalysisParams(undulatorParams, electronBeamParams, FELradiationParams);
+        /*
+        double Lg1D  = FELAnalysisParams.get_FELgainlength1D();
+        double Lg3D  = FELAnalysisParams.get_FELgainlength3D();
+        double rho1D = FELAnalysisParams.get_FELparameter1D ();
+        double rho3D = FELAnalysisParams.get_FELparameter3D ();
+        double Psat  = FELAnalysisParams.get_FELsatpower    ();
+        cout << "Lg1D : " << Lg1D  << " m"  << endl;
+        cout << "Lg3D : " << Lg3D  << " m"  << endl;
+        cout << "rho1D: " << rho1D          << endl;
+        cout << "rho3D: " << rho3D          << endl;
+        cout << "Psat : " << Psat  << " GW" << endl;
+        */
+    }
+
+//    scanpanelParams.info();
+    if (scanpanelParams.get_scanflag()) // do scan job
+    {
+        scanpanelParams.paramScan(varlist,
+                              electronBeamParams,
+                              seedfieldParams,
+                              undulatorParams,
+                              FELradiationParams,
+                              controlpanelParams);
+        scanpanelParams.dumpdata();
+    }
+    else // not scan parameter
+    {
+        //!< do FEL numerical simulation
+        FELNumerical FELNumericalParams(seedfieldParams, undulatorParams, electronBeamParams, FELradiationParams, controlpanelParams);
+        FELNumericalParams.FELsimulation1D();
+        FELNumericalParams.dumpResults();
+    }
+
+    return 0;
+}
